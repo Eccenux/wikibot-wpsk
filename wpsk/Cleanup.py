0a3b9194edf963a6945843419db2b5fc83a6609b
Nux <eccenux+git@gmail.com>
2022-05-15 03:16:43 +0200

    refactor

--- wpsk/Cleanup.py
+++ wpsk/Cleanup.py
@@ -1,105 +1,77 @@
-'''
-	Naprawia parametr język w szablonie Cytuj.
+"""
+	Base script for WP:SK cleanup.
 
 	Makes a backup of before-after contents.
 
-	Should maybe later run more sk-like tasks.
-'''
+	Should eventually run more sk-like tasks.
+"""
 
-from sre_parse import parse_template
-import pywikibot, re
+import pywikibot
 import mwparserfromhell
 from utils.file import *
-
+from wpsk.plugins.Cytuj import Cytuj
 import logging
-logging.basicConfig(filename='logs/lang-fix.log', encoding='utf-8', level=logging.INFO)
 
-site = pywikibot.Site('pl', 'wikipedia')
+class Cleanup:
+	def __init__(self,
+		site: pywikibot.BaseSite,
+		output_path: str
+	):
+		self.site = site
+		self.output_path = output_path
+		self.desc_prefix = 'MiniSK: Poprawiam język w szablonach cytuj'
 
-# config
-output_path = './io/lang-fix'
-test_page_title = 'Wikipedysta:Nux/test_Cytuj_język'
-desc_prefix = 'MiniSK: Poprawiam język w szablonach cytuj'
+	def initdir(dir_path: str, clear=False):
+		logging.info('Preapre dir: %s', dir_path)
 
-def initdir(dir_path, clear=False):
-	logging.info('Preapre dir: %s', dir_path)
+		if clear:
+			# clear dir
+			import shutil
+			shutil.rmtree(dir_path, ignore_errors=True)
 
-	if clear:
-		# clear dir
-		import shutil
-		shutil.rmtree(dir_path, ignore_errors=True)
+		# init dir
+		import os
+		os.makedirs(dir_path, exist_ok=True)
 
-	# init dir
-	import os
-	os.makedirs(dir_path, exist_ok=True)
+	def fix_page(self, page_title: str, dryRun = True):
+		# read content
+		logging.info ('Checking: ' + page_title)
+		print ('Checking: ' + page_title)
+		page = pywikibot.Page(self.site, page_title)
+		page_text: str = page.text
+		page_code: mwparserfromhell.wikicode.Wikicode = mwparserfromhell.parse(page_text)
 
-def fix_lang(template: mwparserfromhell.nodes.template.Template) -> bool:
-	lang_param = 'język'
-	tpl_name = str(template.name).strip().lower()
-	modified = False
-	if tpl_name.startswith('cytuj') and template.has(lang_param):
-		logging.info('Template: %s', tpl_name)
-		lang = template.get(lang_param).value.strip()
-		if lang == 'pl' or lang.startswith('pl-'):
-			template.remove(lang_param)
-			logging.info('\tremoved: %s', lang)
-			modified = True
-		if lang.startswith('en-'):
-			template.add(lang_param, 'en')
-			logging.info('\treplaced: %s', lang)
-			modified = True
-	return modified
+		# find templates with param
+		# https://mwparserfromhell.readthedocs.io/en/latest/api/mwparserfromhell.nodes.html#module-mwparserfromhell.nodes.template
+		fix_count_lang = 0
+		for template in page_code.filter_templates():
+			# before = str(template)
+			modified = Cytuj.fix_lang(template)
+			if modified:
+				fix_count_lang += 1
+				# after = str(template)
+				# print(f'b: {before}\na: {after}')
 
-def fix_page(page_title: str, dryRun = True):
-	# read content
-	logging.info ('Checking: ' + page_title)
-	print ('Checking: ' + page_title)
-	page = pywikibot.Page(site, page_title)
-	page_text: str = page.text
-	page_code: mwparserfromhell.wikicode.Wikicode = mwparserfromhell.parse(page_text)
-
-	# find templates with param
-	# https://mwparserfromhell.readthedocs.io/en/latest/api/mwparserfromhell.nodes.html#module-mwparserfromhell.nodes.template
-	fix_count_lang = 0
-	for template in page_code.filter_templates():
-		# before = str(template)
-		modified = fix_lang(template)
-		if modified:
-			fix_count_lang += 1
-			# after = str(template)
-			# print(f'b: {before}\na: {after}')
-
-	page_text = str(page_code)
-	if (page_text.strip() == page.text.strip()):
-		logging.info('Nothing to do.')
-		print ('Nothing to do.')
-	else:
-		# backup
-		save_page_content(page, output_path, suffix='_before')
-
-		# apply
-		page.text = page_text
-
-		# cmp
-		save_page_content(page, output_path, suffix='_removed')
-
-		# save with description
-		desc = f'{desc_prefix} ({fix_count_lang}).'
-		if dryRun:
-			logging.info(f'Dry-Save "{page.title()}"\n{desc}')
-			print(f'Dry-Save "{page.title()}"\n{desc}')
+		page_text = str(page_code)
+		if (page_text.strip() == page.text.strip()):
+			logging.info('Nothing to do.')
+			print ('Nothing to do.')
 		else:
-			logging.info(f'Save "{page.title()}"\n{desc}')
-			print(f'Save "{page.title()}"\n{desc}')
-			page.save(desc)
+			# backup
+			save_page_content(page, self.output_path, suffix='_before')
 
-# exec
-initdir(output_path)
-#fix_page(test_page_title)
-# copy([...document.querySelectorAll('.mw-search-results a')].map(el=>el.href.replace(/^http.+\//, '')))
-pages = [
-  "Spaso",
-]
-for page_title in pages:
-	fix_page(page_title, dryRun=True)
-	#fix_page(page_title, dryRun=False)
+			# apply
+			page.text = page_text
+
+			# cmp
+			save_page_content(page, self.output_path, suffix='_removed')
+
+			# save with description
+			desc = f'{self.desc_prefix} ({fix_count_lang}).'
+			if dryRun:
+				logging.info(f'Dry-Save "{page.title()}"\n{desc}')
+				print(f'Dry-Save "{page.title()}"\n{desc}')
+			else:
+				logging.info(f'Save "{page.title()}"\n{desc}')
+				print(f'Save "{page.title()}"\n{desc}')
+				page.save(desc)
--- wpsk/plugins/Cytuj.py
+++ wpsk/plugins/Cytuj.py
@@ -1,105 +1,34 @@
-'''
-	Naprawia parametr język w szablonie Cytuj.
+"""
+	Korekty szablonu Cytuj.
+"""
 
-	Makes a backup of before-after contents.
-
-	Should maybe later run more sk-like tasks.
-'''
-
-from sre_parse import parse_template
-import pywikibot, re
 import mwparserfromhell
 from utils.file import *
-
 import logging
-logging.basicConfig(filename='logs/lang-fix.log', encoding='utf-8', level=logging.INFO)
-
-site = pywikibot.Site('pl', 'wikipedia')
 
 # config
 output_path = './io/lang-fix'
 test_page_title = 'Wikipedysta:Nux/test_Cytuj_język'
 desc_prefix = 'MiniSK: Poprawiam język w szablonach cytuj'
 
-def initdir(dir_path, clear=False):
-	logging.info('Preapre dir: %s', dir_path)
+class Cytuj:
+	def fix_lang(template: mwparserfromhell.nodes.template.Template) -> bool:
+		"""
+		Naprawia parametr język w szablonach cytuj*.
+		"""
 
-	if clear:
-		# clear dir
-		import shutil
-		shutil.rmtree(dir_path, ignore_errors=True)
-
-	# init dir
-	import os
-	os.makedirs(dir_path, exist_ok=True)
-
-def fix_lang(template: mwparserfromhell.nodes.template.Template) -> bool:
-	lang_param = 'język'
-	tpl_name = str(template.name).strip().lower()
-	modified = False
-	if tpl_name.startswith('cytuj') and template.has(lang_param):
-		logging.info('Template: %s', tpl_name)
-		lang = template.get(lang_param).value.strip()
-		if lang == 'pl' or lang.startswith('pl-'):
-			template.remove(lang_param)
-			logging.info('\tremoved: %s', lang)
-			modified = True
-		if lang.startswith('en-'):
-			template.add(lang_param, 'en')
-			logging.info('\treplaced: %s', lang)
-			modified = True
-	return modified
-
-def fix_page(page_title: str, dryRun = True):
-	# read content
-	logging.info ('Checking: ' + page_title)
-	print ('Checking: ' + page_title)
-	page = pywikibot.Page(site, page_title)
-	page_text: str = page.text
-	page_code: mwparserfromhell.wikicode.Wikicode = mwparserfromhell.parse(page_text)
-
-	# find templates with param
-	# https://mwparserfromhell.readthedocs.io/en/latest/api/mwparserfromhell.nodes.html#module-mwparserfromhell.nodes.template
-	fix_count_lang = 0
-	for template in page_code.filter_templates():
-		# before = str(template)
-		modified = fix_lang(template)
-		if modified:
-			fix_count_lang += 1
-			# after = str(template)
-			# print(f'b: {before}\na: {after}')
-
-	page_text = str(page_code)
-	if (page_text.strip() == page.text.strip()):
-		logging.info('Nothing to do.')
-		print ('Nothing to do.')
-	else:
-		# backup
-		save_page_content(page, output_path, suffix='_before')
-
-		# apply
-		page.text = page_text
-
-		# cmp
-		save_page_content(page, output_path, suffix='_removed')
-
-		# save with description
-		desc = f'{desc_prefix} ({fix_count_lang}).'
-		if dryRun:
-			logging.info(f'Dry-Save "{page.title()}"\n{desc}')
-			print(f'Dry-Save "{page.title()}"\n{desc}')
-		else:
-			logging.info(f'Save "{page.title()}"\n{desc}')
-			print(f'Save "{page.title()}"\n{desc}')
-			page.save(desc)
-
-# exec
-initdir(output_path)
-#fix_page(test_page_title)
-# copy([...document.querySelectorAll('.mw-search-results a')].map(el=>el.href.replace(/^http.+\//, '')))
-pages = [
-  "Spaso",
-]
-for page_title in pages:
-	fix_page(page_title, dryRun=True)
-	#fix_page(page_title, dryRun=False)
+		lang_param = 'język'
+		tpl_name = str(template.name).strip().lower()
+		modified = False
+		if tpl_name.startswith('cytuj') and template.has(lang_param):
+			logging.info('Template: %s', tpl_name)
+			lang = template.get(lang_param).value.strip()
+			if lang == 'pl' or lang.startswith('pl-'):
+				template.remove(lang_param)
+				logging.info('\tremoved: %s', lang)
+				modified = True
+			if lang.startswith('en-'):
+				template.add(lang_param, 'en')
+				logging.info('\treplaced: %s', lang)
+				modified = True
+		return modified
