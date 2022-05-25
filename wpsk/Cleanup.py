9ee37a8afd1267c367eae92cb8f15fc2cdf5df7d
Nux <eccenux+git@gmail.com>
2022-05-26 01:24:14 +0200

    summary separation

--- wpsk/Cleanup.py
+++ wpsk/Cleanup.py
@@ -21,7 +21,7 @@ class Cleanup:
 	):
 		self.site = site
 		self.output_path = output_path
-		self.desc_prefix = 'MiniSK: Poprawiam język w szablonach cytuj'
+		self.desc_prefix = 'MiniSK:'
 
 	def initdir(dir_path: str, clear=False):
 		logging.info('Preapre dir: %s', dir_path)
@@ -42,38 +42,45 @@ class Cleanup:
 		page = pywikibot.Page(self.site, page_title)
 		page_text: str = page.text
 		page_code: Wikicode = mwparserfromhell.parse(page_text)
-		fix_count_lang = self._fix_code(page_code)
-		if fix_count_lang > 1:
-			self._apply(page, page_code, fix_count_lang, dryRun)
+		(fix_count, summary) = self._fix_code(page_code)
+		if fix_count > 1:
+			self._apply(page, page_code, summary, dryRun)
 
 	def _fix_code(self,
 		page_code: Wikicode,
 	):
 		# fixes by type
-		fix_count_lang = 0
+		fix_count = 0
+		summary = []
 		
 		# templates
+		fix_count_lang = 0
 		for template in page_code.filter_templates():
 			modified = Cytuj.fix_lang(template)
 			if modified:
 				fix_count_lang += 1
+		if fix_count_lang > 0:
+			fix_count += fix_count_lang
+			summary.append(f'Język w Cytuj* ({fix_count_lang})')
 		
 		# text
+		quotesPl = QuotesPl(page_code)
 		for text_node in page_code.filter_text():
 			# skip in templates (directly in templates)
 			parent = page_code.get_parent(text_node)
 			if isinstance(parent, mwparserfromhell.nodes.template.Template):
 				continue
-			modified = QuotesPl.fix(text_node, page_code)
-			if modified:
-				fix_count_lang += 1
+			quotesPl.fix(text_node)
+		if quotesPl.count() > 0:
+			fix_count += quotesPl.count()
+			summary.append(quotesPl.summary())
 		
-		return fix_count_lang
+		return (fix_count, summary)
 
 	def _apply(self,
 		page: pywikibot.Page,
 		page_code: Wikicode,
-		fix_count_lang: int,
+		summary: list,
 		dryRun: True,
 	):
 		page_text = str(page_code)
@@ -91,15 +98,15 @@ class Cleanup:
 			save_page_content(page, self.output_path, suffix='_removed')
 
 			# ~save
-			self._save(page, fix_count_lang, dryRun)
+			self._save(page, summary, dryRun)
 
 	def _save(self,
 		page: pywikibot.Page,
-		fix_count_lang: int,
+		summary: list,
 		dryRun: True,
 	):
 		# save with description
-		desc = f'{self.desc_prefix} ({fix_count_lang}).'
+		desc = f'{self.desc_prefix} {", ".join(summary)}.'
 		if dryRun:
 			logging.info(f'Dry-Save "{page.title()}"\n{desc}')
 			print(f'Dry-Save "{page.title()}"\n{desc}')
